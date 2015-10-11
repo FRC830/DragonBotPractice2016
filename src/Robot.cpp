6d3ad6478c58d288890ca8e2bb8ebea2c3125cb4
werehyena <morton.scott.e@gmail.com>
2015-10-11 15:26:48 -0400

    add submodule, teleop

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,9 +1,10 @@
 #include "WPILib.h"
 
+#include "../utils/830utilities.h"
+
 class Robot: public IterativeRobot
 {
 private:
-	LiveWindow *lw;
 	static const int EYE_PWM = 0;
 	static const int FRONT_LEFT_PWM = 1;
 	static const int FRONT_RIGHT_PWM = 2;
@@ -14,11 +15,52 @@ private:
 	static const int HEAD_MOTOR_PWM = 7;
 	static const int WING_FLAP_PWM = 8;
 	static const int WING_FOLD_PWM = 9;
+//	static const int SOUND_1 = 8;
+//	static const int SOUND_2 = 2;
 
+	RobotDrive *drive;
+	GamepadF310 *pilot;
+	GamepadF310 *copilot;
+//	DigitalOutput *sound1;
+//	DigitalOutput *sound2;
+	DigitalOutput *sounds[10];
 	void RobotInit()
 	{
-		lw = LiveWindow::GetInstance();
+
+		pilot = new GamepadF310(0);
+
+		copilot = new GamepadF310(1);
+
+		drive = new RobotDrive(
+			new Victor(FRONT_LEFT_PWM),
+			new Victor(BACK_LEFT_PWM),
+			new Victor(FRONT_RIGHT_PWM),
+			new Victor(BACK_RIGHT_PWM)
+		);
+		drive->SetInvertedMotor(RobotDrive::kFrontRightMotor, true);
+		drive->SetInvertedMotor(RobotDrive::kRearRightMotor, true);
+		for (int i = 0; i < 10; i++)
+			sounds[i]= new DigitalOutput(i);
+//		sound1 = new DigitalOutput(SOUND_1);
+//		sound2 = new DigitalOutput(SOUND_2);
+//		sound1->Set(1);
+//		sound2->Set(1);
 	}
+
+
+	void PlaySound (int id) {
+		for (int i = 0; i < 10; i++)
+			sounds[i]->Set(i != id);
+	}
+	void StopSound () {
+		for (int i = 0; i < 10; i++)
+			sounds[i]->Set(1);
+	}
+
+	void DisabledInit(){
+		StopSound();
+	}
+	void DisabledPeriodic(){}
 
 	void AutonomousInit()
 	{
@@ -37,13 +79,24 @@ private:
 
 	void TeleopPeriodic()
 	{
+		PlaySound(1);
+		if(pilot ->DPadX()==1){
+			PlaySound(8);
+		}
+		else {
+			StopSound();
+		}
+		float speed=3;
+		if(pilot->ButtonState(F310Buttons::LeftStick))
+			speed=1.25;
+		drive->MecanumDrive_Cartesian(
+				pilot->LeftX()/speed,
+				-pilot->LeftY()/speed,
+				pilot->RightX()/speed
 
+		);
 	}
 
-	void TestPeriodic()
-	{
-		lw->Run();
-	}
 };
 
 START_ROBOT_CLASS(Robot);
